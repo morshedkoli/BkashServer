c38561021d7c374663cb4d9da6a73bbb05d49f9b
MORSHED AL MAIN <murshedkoli@gmail.com>
2024-08-23 04:56:04 +0600

    action disable

--- actions/fatchTransactions.ts
+++ actions/fatchTransactions.ts
@@ -1,6 +1,6 @@
-export const fetchAllTransactions = () => {
-  const res = fetch(`${process.env.HOST}/api/transection/admin/all`);
-  const data = res.json();
+// export const fetchAllTransactions = () => {
+//   const res = fetch(`${process.env.HOST}/api/transection/admin/all`);
+//   const data = res.json();
 
-  return data;
-};
+//   return data;
+// };
